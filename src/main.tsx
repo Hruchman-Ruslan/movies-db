dffcf081df84a4464ce27ea17e4b56674e79dbca
Ruslan <Giovingo92@gmail.com>
2024-04-06 20:55:22 +0300

    feat: router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import App from "./App.tsx";
+import "./index.css";
+import About from "./page/About.tsx";
+import Movies from "./page/Movies.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/movies-db/",
+    element: <App />,
+    children: [
+      {
+        path: "/movies-db/about",
+        element: <About />,
+      },
+      {
+        path: "/movies-db/movies",
+        element: <Movies />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
