a89d3d77cd529f568532f3e7d8ba4b7f7fd61640
Ruslan <Giovingo92@gmail.com>
2024-04-11 23:38:14 +0300

    feat: useRef

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,7 @@
 
 export * from "./MovieCard/MovieCard";
 export * from "./AppHeader/AppHeader";
+export * from "./CountdownText/CountdownText";
+export * from "./CountdownVideo/CountdownVideo";
 
 // css
--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,7 +1,12 @@
+import { Container } from "@mui/material";
+
+import { CountdownText, CountdownVideo } from "../components";
+
 export default function About() {
   return (
-    <>
-      <div>About</div>
-    </>
+    <Container sx={{ py: 8 }} maxWidth="md">
+      <CountdownText />
+      <CountdownVideo />
+    </Container>
   );
 }
