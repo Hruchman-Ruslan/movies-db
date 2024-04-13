2963eec8c026a3acf8c2544834ebb62c56540540
Ruslan <Giovingo92@gmail.com>
2024-04-13 21:55:40 +0300

    feat: useCallback useMemo

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 // html
 
-export * from "./MovieCard/MovieCard";
+// export * from "./MovieCard/MovieCard";
 export * from "./AppHeader/AppHeader";
 export * from "./CountdownText/CountdownText";
 export * from "./CountdownVideo/CountdownVideo";
